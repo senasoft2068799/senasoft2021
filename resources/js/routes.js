a547f9df38710d128c34528b5072cd57cf380ba2
Ju4nPx <juanpmachadob@gmail.com>
2021-10-20 15:48:44 -0500

    Modificacion de rutas

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,6 +1,5 @@
 import Index from "./pages/Index.vue";
 import Auth from "./pages/Auth/Auth.vue";
-import Bienvenido from "./pages/Bienvenido.vue";
 
 export const routes = [
     {
@@ -13,11 +12,5 @@ export const routes = [
         name: "auth",
         path: "/auth",
         component: Auth,
-    },
-    {
-        name: "Bienvenido",
-        path: "/bienvenido",
-        component: Bienvenido,
-        meta: { requiresAuth: true }
     },
 ];
