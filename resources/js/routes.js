a58ebacd44543ab310714dd48ec81085423b4270
scalarcon <camilo-lara01@hotmail.com>
2021-10-21 09:21:02 -0500

    tablero

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,7 +1,8 @@
 import Index from "./pages/Index.vue";
 import Auth from "./pages/Auth/Auth.vue";
-import Bienvenido from "./pages/Bienvenido.vue";
 import Sala from "./pages/Sala.vue";
+import Reglas from "./pages/Reglas.vue";
+import Tablero from "./components/Tablero.vue";
 
 export const routes = [
     {
@@ -16,15 +17,21 @@ export const routes = [
         component: Auth,
     },
     {
-        name: "Bienvenido",
-        path: "/bienvenido",
-        component: Bienvenido,
-        meta: { requiresAuth: true }
-    },
-    {
         name: "Sala",
         path: "/sala",
         component: Sala,
+        meta: { requiresAuth: true }
+    },
+    {
+        name: "Reglas",
+        path: "/reglas",
+        component: Reglas,
+        meta: { requiresAuth: true }
+    },
+    {
+        name: "Tablero",
+        path: "/tablero",
+        component: Tablero,
         meta: { requiresAuth: true }
     },
 ];
