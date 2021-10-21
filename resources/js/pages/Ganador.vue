09505e2bed5d91aa5614490022c85eb3d53a0eb3
kevinruiz1811 <kevin.ruiz1811@gmail.com>
2021-10-21 10:43:14 -0500

    Página de ganador

--- resources/js/pages/Ganador.vue
+++ resources/js/pages/Ganador.vue
@@ -9,26 +9,10 @@
         <h1 style="color: white">Nombre del jugador</h1>
         <i class="fas fa-trophy" style="color: white; font-size: 100px"></i>
         <h2 style="color: white">
-          ¡Felicidades "nombreJugador", eres el ganador del día de hoy!
+          ¡Felicidades "nombreJugador", eres el ganador de la partida!
         </h2>
-        <h2 style="color: white">¿Ahora qué deseas hacer?</h2>
-        <div style="display: flex;">
-          <div class="col-md-4">
-            <button
-              @click="crearPartida()" class="botonGanador"
-            >
-              Crear partida
-              <i class="fas fa-gamepad"></i>
-            </button>
-          </div>
-          <div class="col-md-4">
-            <button
-              @click="crearPartida()" class="botonGanador"
-            >
-              Unirse a partida <i class="fas fa-sign-in-alt"></i>
-            </button>
-          </div>
-          <div class="col-md-4">
+        <div>
+          <div class="col-md-12">
             <button class="botonGanador">
               <router-link
                 :to="{ name: 'index' }"
@@ -44,33 +28,6 @@
 </template>
 <script>
 export default {
-  data() {
-    return {
-      partida: {
-        jugadore_id: 1,
-      },
-    };
-  },
-  methods: {
-    crearPartida() {
-      this.$swal({
-        title: "Creando partida...",
-      });
-      this.$swal.showLoading();
-      axios
-        .post("/api/partida", this.partida)
-        .then((res) => {
-          Storage.record("partida", res.data, false);
-          this.$swal.close();
-          this.$router.push("/sala");
-        })
-        .catch((err) => {
-          this.$swal({
-            icon: "error",
-            title: "Ha ocurrido un error:\n" + err,
-          });
-        });
-    },
-  },
+  
 };
 </script>
