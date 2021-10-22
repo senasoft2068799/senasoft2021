84d538816c27e9870fac52c3fb33f5275dcead49
kevinruiz1811 <kevin.ruiz1811@gmail.com>
2021-10-22 10:52:55 -0500

    Form preguntas en acusación

--- resources/js/components/Partida/Acusacion.vue
+++ resources/js/components/Partida/Acusacion.vue
@@ -1,12 +1,11 @@
 <template>
   <div>
+    <FormPreguntaAcusacion :cartasSeleccionadas="cartasSeleccionadas" />
     <center>
       <div class="row-cols-1 row-cols-md-3 g-4 mt-3">
         <div class="contenedor-preguntas">
-          <input type="radio" name="dot" id="uno" />
-          <input type="radio" name="dot" id="dos" />
-          <input type="radio" name="dot" id="tres" />
-          <PanelCartas />
+          <h1 style="color: white;">¡Selecciona las cartas que crees que son las que están ocultas!</h1>
+          <PanelCartas @seleccionar="seleccionarCarta($event)"/>
         </div>
       </div>
     </center>
@@ -14,14 +13,31 @@
 </template>
 <script>
 import PanelCartas from "../Preguntas/PanelCartas.vue";
+import FormPreguntaAcusacion from "../Preguntas/FormPreguntasAcusacion.vue";
 export default {
   components: {
     PanelCartas,
+    FormPreguntaAcusacion,
   },
   data() {
     return {
-      
+      cartasSeleccionadas: {
+        programador: null,
+        modulo: null,
+        error: null,
+      },
     };
+  },
+  methods: {
+    seleccionarCarta(event) {
+      if (event.tipo == 1) {
+        this.cartasSeleccionadas.programador = event;
+      } else if (event.tipo == 2) {
+        this.cartasSeleccionadas.modulo = event;
+      } else if (event.tipo == 3) {
+        this.cartasSeleccionadas.error = event;
+      }
+    },
   },
 };
 </script>
---
+++ resources/js/components/Preguntas/FormPreguntasAcusacion.vue
@@ -0,0 +1,55 @@
+<template>
+  <form
+    class="form"
+    style="
+      padding-top: 0px;
+      margin-top: 0px;
+      max-width: 880px;
+      width: 80%;
+      height: 50px;
+    "
+  >
+    <div class="form-container" style="margin-top: 30px">
+      <div class="form-group" style="display: flex">
+        <h2 class="h2Preguntas">El programador</h2>
+        <input
+          type="text"
+          class="form-input inputPreguntas"
+          disabled
+          v-if="cartasSeleccionadas.programador"
+          v-model="cartasSeleccionadas.programador.nombre"
+        />
+        <input type="text" class="form-input" disabled v-else />
+        <h2 class="h2Preguntas">generó en el módulo</h2>
+        <input
+          type="text"
+          class="form-input inputPreguntas"
+          disabled
+          v-if="cartasSeleccionadas.modulo"
+          v-model="cartasSeleccionadas.modulo.nombre"
+        />
+        <input type="text" class="form-input" disabled v-else />
+        <h2 class="h2Preguntas">un error</h2>
+        <input
+          type="text"
+          class="form-input inputPreguntas"
+          disabled
+          v-if="cartasSeleccionadas.error"
+          v-model="cartasSeleccionadas.error.nombre"
+        />
+        <input type="text" class="form-input" disabled v-else />
+        <button type="submit" class="botonPreguntas">Realizar acusacion</button>
+      </div>
+    </div>
+  </form>
+</template>
+<script>
+import Cartas from "../Cartas.vue";
+export default {
+  name: "FormPreguntaAcusacion",
+  props: ["cartasSeleccionadas"],
+  components: {
+    Cartas,
+  },
+};
+</script>
