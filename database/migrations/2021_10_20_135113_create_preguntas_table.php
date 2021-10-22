a9789c21bd5f21c2e85c3fb434fabb722b70c98b
Ju4nPx <juanpmachadob@gmail.com>
2021-10-22 09:31:37 -0500

    Error migracion corregido

--- database/migrations/2021_10_20_135113_create_preguntas_table.php
+++ database/migrations/2021_10_20_135113_create_preguntas_table.php
@@ -6,11 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 class CreatePreguntasTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('preguntas', function (Blueprint $table) {
