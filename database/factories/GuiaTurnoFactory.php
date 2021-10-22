ff49349d040333bfb9aefc15211a53b293fe63c1
Ju4nPx <juanpmachadob@gmail.com>
2021-10-22 08:58:02 -0500

    Estilos kevin

---
+++ database/factories/GuiaTurnoFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\GuiaTurno;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class GuiaTurnoFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = GuiaTurno::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/migrations/2021_10_20_033659_create_guia_turnos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGuiaTurnosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('guia_turnos', function (Blueprint $table) {
+            $table->id();
+            $table->string("pregunta_user_nickname");
+            $table->string("respuesta_user_nickname");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('guia_turnos');
+    }
+}
